ce13d55d9d25b88c04e86d07aaf8cf23c5442d0a
Sascha Wu <sascha.wu@protonmail.com>
2021-08-14 18:38:22 -0400

    initial svelte files

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Description from './Description.svelte';
+	export let name;
+</script>
+
+<main>
+	<h1>deleting the in-between / WHAT ARE YOU DOING HERE</h1>
+	<Description />
+	<div class="circle" id="circle1" />
+	<div class="circle" id="circle2" />
+</main>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		z-index: 100;
+	}
+
+	h1 {
+		color: blue;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.circle {
+		position: absolute;
+		border: yellow 20px solid;
+		border-radius: 100%;
+		width: 30vw;
+		height: 30vw;
+		z-index: -1;
+	}
+	#circle1 {
+		top: 0;
+	}
+
+	#circle2 {
+		top: 20vh;
+		left: 20vw;
+
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Description.svelte
@@ -0,0 +1,15 @@
+<section>
+<p>setting up my own website is very time consuming and I'm pretty much exhausted all the time.</p>
+
+<p>I have better things to do like overshare on Facebook ðŸ™ƒ</p>
+
+	<p>knowing me, this thing will forever be under construction - so i forced this shit-eating goblin to make it for me</p>
+
+
+	<p>for now, check out some stuff I did, or tried to do:</p>
+
+	<p>The Sketchbook Project - artwork that calls the Brooklyn Art Library home. my physical sketchbook is currently on tour! chananigans - an idea that happened with a friend because NYC transit is oftentimes annoying. starberry - cHecK oUT mY sOunDCL0ud!! hahA it's ok I guess but it could be better,,,</p>
+	<p>if for some weird ass reason you want to get in touch with me to tell me how terrible and unprofessional I am, please message me via social media. alternatively, if you were gifted one of my 'business' cards with original art on the back, you have too much of my information already... please spare me? thanks</p>
+
+	<p>oh yeah! here's some more of my art...</p>
+</section>
