caf4e5b46d623b1676785a103acc4589e72b306d
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2023-08-25 17:17:49 -0400

    add linters

---
+++ src/SideNav.svelte
@@ -0,0 +1,13 @@
+
+<div class="text-tile">
+	{#each projects as project}
+		<li>
+			{#if project.name === "Betty" || project.name === "chananigans"}
+			<a href={project.name.toLowerCase()} use:link>{project.name}</a>
+			{:else}
+			<a target="_blank" href={project.link}>{project.name}</a>
+			{/if}
+			- {project.description}
+		</li>
+	{/each}
+</div>
