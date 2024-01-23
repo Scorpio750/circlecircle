0b4079fd7188b5157d5205e43783d18a4627944c
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2024-01-23 16:27:51 -0500

    add new pages and fonts

---
+++ src/Portfolio.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Carousel from 'svelte-carousel';
+</script>
+
+<section class="hero">
+	<div id="carousel">
+		<Carousel
+			autoplay
+			autoplayDuration={5000}
+			arrows={false}
+			particlesToShow={3}
+			particlesToScroll={2}
+		>
+			<Image src="./assets/Betty/A Cloudy Day by the Sea.jpeg" />
+			<Image src="./assets/Betty/Cactus Dance II.jpeg" />
+			<Image src="./assets/Betty/Cactus Dance.jpeg" />
+		</Carousel>
+	</div>
+</section>
+
