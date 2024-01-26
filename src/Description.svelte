5bbb4d0123d2898f0b856810c7f55b22624d0fbd
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2024-01-26 01:48:19 -0500

    add all images to carousel

--- src/Description.svelte
+++ src/Description.svelte
@@ -4,11 +4,52 @@
 	import Image from './components/Image.svelte';
 </script>
 
+<table class="roundy" style="float: right; background: #A8A878; margin-left: 10px; margin-bottom: 10px; border: 3px solid #6D6D4E; max-width: 430px !important;">
+	<tbody><tr style="text-align:center">
+			<td colspan="2"><big><big><b>Copycat (Bo)</b></big></big><br><span lang="ja"><b>モノマネむすめ</b></span> <i>Mimic Girl</i>
+		</td></tr>
+		<tr style="text-align:center">
+			<td class="roundy" style="background: #FFF" colspan="2"><a href="/wiki/File:CopycatLGPE.png" class="image"><img alt="CopycatLGPE.png" src="http://archives.bulbagarden.net/media/upload/e/ea/CopycatLGPE.png" decoding="async" width="250" height="292"></a><br><small>Screenshot of Copycat from <a href="/wiki/Pok%C3%A9mon:_Let%27s_Bo,_Pikachu!_and_Let%27s_Bo,_Eevee!" title="Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!">Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!</a></small>
+			</td></tr>
+			<tr>
+				<th class="roundytl roundybl" style="text-align:right; background: #C6C6A7"><a href="/wiki/Gender" title="Gender"><span style="color:#000;">Gender</span></a>
+				</th>
+				<td class="roundytr roundybr" style="background:#FFF">Female(?)
+	</td></tr>
+	<tr>
+		<th class="roundytl" style="text-align:right; background: #C6C6A7"><span style="color:#000;">Hometown</span>
+		</th>
+		<td class="roundytr" style="background:#FFF">Chinatown
+	</td></tr>
+	<tr>
+		<th class="" style="text-align:right; background: #C6C6A7"><a href="/wiki/Region" title="Region"><span style="color:#000;">Region</span></a>
+		</th>
+		<td class="" style="background:#FFF">Canto
+	</td></tr>
+	<tr>
+		<th class="roundybl" style="text-align:right; background: #C6C6A7">Relatives
+		</th>
+	<td class="roundybr" style="background:#FFF">Unnamed father, unnamed grandmother
+	</td></tr>
+	<tr>
+		<th class="roundytl" style="text-align:right; background: #C6C6A7">Generation
+		</th>
+		<td class="roundytr" style="background:#FFF">
+			Millenial
+	</td></tr>
+	<tr>
+		<th class="roundybl" style="text-align:right; background: #C6C6A7">Description
+		</th>
+		<td class="roundybr" style="background:#FFF">
+			Bo is a mixed media artist from New York City. Her works are based on the flow and folding of time, the forgotten feelings and thoughts of the moments gone by. She draws inspiration from organic forms found within nature, in addition to the temporality of street art and the buildings they belong to.
+	</td></tr>
+</tbody></table>
 <div id="description-section">
-		<div class="text-tile">
- Bo is a mixed media artist from New York City. Her works are based on the flow and folding of time, the forgotten feelings and thoughts of the moments gone by. She draws inspiration from organic forms found within nature, in addition to the temporality of street art and the buildings they belong to
-		</div>
+	<div class="text-tile">
+	</div>
 </div>
 
 <style>
+	#description-section {
+	}
 </style>
--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -9,13 +9,47 @@
 			autoplay
 			autoplayDuration={5000}
 			arrows={false}
-			particlesToShow={1}
+			particlesToShow={2}
 			particlesToScroll={2}
 		>
 			<Image src="./assets/Betty/A Cloudy Day by the Sea.jpeg" />
 			<Image src="./assets/Betty/Cactus Dance II.jpeg" />
 			<Image src="./assets/Betty/Cactus Dance.jpeg" />
+			<Image src="./assets/Betty/Cloud Garden.jpeg" />
+			<Image src="./assets/Betty/Colorcruise.jpeg" />
+			<Image src="./assets/Betty/Cosmic Dust.jpeg" />
+			<Image src="./assets/Betty/Darkness Creeps In.jpeg" />
+			<Image src="./assets/Betty/Deathstar.jpeg" />
+			<Image src="./assets/Betty/Discovery.jpeg" />
+			<Image src="./assets/Betty/Diver.jpeg" />
+			<Image src="./assets/Betty/Everything Dies.jpeg" />
+			<Image src="./assets/Betty/Falling Off the Canvas.jpeg" />
+			<Image src="./assets/Betty/Flora.jpeg" />
+			<Image src="./assets/Betty/Funnel.jpeg" />
+			<Image src="./assets/Betty/Jitters.jpeg" />
+			<Image src="./assets/Betty/Kestrel.jpeg" />
+			<Image src="./assets/Betty/Kineticity.jpeg" />
+			<Image src="./assets/Betty/King Frog.jpeg" />
+			<Image src="./assets/Betty/Last of the Dinosaurs.jpeg" />
+			<Image src="./assets/Betty/Mecharoot.jpeg" />
+			<Image src="./assets/Betty/Mesh Potatoes.jpeg" />
+			<Image src="./assets/Betty/New Perspective.jpeg" />
+			<Image src="./assets/Betty/Roads that Lead to Something.jpeg" />
+			<Image src="./assets/Betty/Serpentine.jpeg" />
+			<Image src="./assets/Betty/Shirley & the Medium Hypnosis.jpeg" />
+			<Image src="./assets/Betty/Space Junk.jpeg" />
+			<Image src="./assets/Betty/Tangle.jpeg" />
+			<Image src="./assets/Betty/the-end.jpeg" />
+			<Image src="./assets/Betty/This Could Be Sexual.jpeg" />
+			<Image src="./assets/Betty/Unstructural Sound.jpeg" />
+			<Image src="./assets/Betty/Valleys & Peaks.jpeg" />
+			<Image src="./assets/Betty/Yeeted into the Abyss.jpeg" />
 		</Carousel>
 	</div>
 </section>
 
+<style>
+	#carousel {
+		max-width: 33vw;
+	}
+</style>
