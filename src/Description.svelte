ece0158dd2444752d64cdef5ac111c76ee3afce4
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2024-01-28 04:25:41 -0500

    egg

--- src/Description.svelte
+++ src/Description.svelte
@@ -4,43 +4,43 @@
 	import Image from './components/Image.svelte';
 </script>
 
-<table class="roundy" style="float: right; background: #A8A878; margin-left: 10px; margin-bottom: 10px; border: 3px solid #6D6D4E; max-width: 430px !important;">
+<table style="float: right; padding: 4em; background: lightgoldenrodyellow; margin-left: 10px; margin-bottom: 10px; border: 3px solid #6D6D4E; border-radius: 100%; max-width: 430px !important;">
 	<tbody><tr style="text-align:center">
 			<td colspan="2"><big><big><b>Copycat (Bo)</b></big></big><br><span lang="ja"><b>モノマネむすめ</b></span> <i>Mimic Girl</i>
 		</td></tr>
 		<tr style="text-align:center">
-			<td class="roundy" style="background: #FFF" colspan="2"><a href="/wiki/File:CopycatLGPE.png" class="image"><img alt="CopycatLGPE.png" src="http://archives.bulbagarden.net/media/upload/e/ea/CopycatLGPE.png" decoding="async" width="250" height="292"></a><br><small>Screenshot of Copycat from <a href="/wiki/Pok%C3%A9mon:_Let%27s_Bo,_Pikachu!_and_Let%27s_Bo,_Eevee!" title="Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!">Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!</a></small>
+			<td style="background: #FFF" colspan="2"><a href="/wiki/File:CopycatLGPE.png" class="image"><img alt="CopycatLGPE.png" src="http://archives.bulbagarden.net/media/upload/e/ea/CopycatLGPE.png" decoding="async" width="250" height="292"></a><br><small>Screenshot of Copycat from <a href="/wiki/Pok%C3%A9mon:_Let%27s_Bo,_Pikachu!_and_Let%27s_Bo,_Eevee!" title="Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!">Pokémon: Let's Bo, Pikachu! and Let's Bo, Eevee!</a></small>
 			</td></tr>
 			<tr>
-				<th class="roundytl roundybl" style="text-align:right; background: #C6C6A7"><a href="/wiki/Gender" title="Gender"><span style="color:#000;">Gender</span></a>
+				<th class="roundytl roundybl" style="text-align:right; overflow: hidden"><a href="/wiki/Gender" title="Gender"><span style="color:#000;">Gender</span></a>
 				</th>
-				<td class="roundytr roundybr" style="background:#FFF">Female(?)
+				<td class="roundytr roundybr" style="overflow: hidden">Female
 	</td></tr>
 	<tr>
-		<th class="roundytl" style="text-align:right; background: #C6C6A7"><span style="color:#000;">Hometown</span>
+		<th style="text-align:right; overflow: hidden"><span style="color:#000;">Hometown</span>
 		</th>
-		<td class="roundytr" style="background:#FFF">Chinatown
+		<td style="overflow: hidden">Chinatown
 	</td></tr>
 	<tr>
-		<th class="" style="text-align:right; background: #C6C6A7"><a href="/wiki/Region" title="Region"><span style="color:#000;">Region</span></a>
+		<th class="" style="text-align:right; overflow: hidden"><a href="/wiki/Region" title="Region"><span style="color:#000;">Region</span></a>
 		</th>
-		<td class="" style="background:#FFF">Canto
+		<td class="" style="overflow: hidden">Canto
 	</td></tr>
 	<tr>
-		<th class="roundybl" style="text-align:right; background: #C6C6A7">Relatives
+		<th style="text-align:right; overflow: hidden">Relatives
 		</th>
-	<td class="roundybr" style="background:#FFF">Unnamed father, unnamed grandmother
+	<td style="overflow: hidden">Unnamed father, unnamed grandmother
 	</td></tr>
 	<tr>
-		<th class="roundytl" style="text-align:right; background: #C6C6A7">Generation
+		<th style="text-align:right; overflow: hidden">Generation
 		</th>
-		<td class="roundytr" style="background:#FFF">
+		<td style="overflow: hidden">
 			Millenial
 	</td></tr>
 	<tr>
-		<th class="roundybl" style="text-align:right; background: #C6C6A7">Description
+		<th style="text-align:right; overflow: hidden">Description
 		</th>
-		<td class="roundybr" style="background:#FFF">
+		<td style="overflow: hidden">
 			Bo is a mixed media artist from New York City. Her works are based on the flow and folding of time, the forgotten feelings and thoughts of the moments gone by. She draws inspiration from organic forms found within nature, in addition to the temporality of street art and the buildings they belong to.
 	</td></tr>
 </tbody></table>
