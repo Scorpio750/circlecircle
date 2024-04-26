5b532802dc73828a1d2af271f5d24b0f4f829e41
Sascha Wu <6061507+Syncretik@users.noreply.github.com>
2024-04-26 12:17:06 -0400

    make social links open in new tab

--- src/SocialLinks.svelte
+++ src/SocialLinks.svelte
@@ -8,16 +8,16 @@
 <section>
 	<h1>Social Links</h1>
 	<div id="social-links">
-		<a class="social-link" href="https://www.instagram.com/copy____cat___/">
+		<a class="social-link" href="https://www.instagram.com/copy____cat___/" target="_blank">
 			<InstagramSVG />
 		</a>
-		<a  class="social-link" href="https://www.facebook.com/facebo/">
+		<a  class="social-link" href="https://www.facebook.com/facebo/" target="_blank">
 			<FacebookSVG />
 		</a>
-		<a  class="social-link" href="https://soundcloud.com/capybo">
+		<a  class="social-link" href="https://soundcloud.com/capybo" target="_blank">
 			<SoundcloudSVG />
 		</a>
-		<a class="social-link" href="https://paypal.me/circlecirclestudio">
+		<a class="social-link" href="https://paypal.me/circlecirclestudio" target="_blank">
 			<PaypalSVG />
 		</a>
 	</div>
